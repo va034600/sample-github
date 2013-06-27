89d016400a58ee804451ad47bd0c6c90d99ea6c8
eisakuu <eisakuu@mac>
2013-06-28 08:12:59 +0900

    まずコミット

---
+++ src/main/java/sample/junit4/MessageUtility.java
@@ -0,0 +1,10 @@
+package sample.junit4;
+
+public class MessageUtility {
+	private MessageUtility() {
+	}
+
+	public static final String getMessage() {
+		return "abc";
+	}
+}
---
+++ src/test/java/sample/junit4/MessageUtilityTest.java
@@ -0,0 +1,14 @@
+package sample.junit4;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class MessageUtilityTest {
+	@Test
+	public void testGetMessage_01() throws IOException {
+		assertEquals("abc", MessageUtility.getMessage());
+	}
+}
