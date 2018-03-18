f2c59a2f110a0873126fd88c57e340cf22bb2229
va034600 <va034600@gmail.com>
2018-03-19 07:00:32 +0900

    3 (+2 squashed commits) Squashed commits: [e4d8dd2] 2 [ed4008b] 1

--- src/main/java/sample/junit4/MessageUtility.java
+++ src/main/java/sample/junit4/MessageUtility.java
@@ -4,9 +4,9 @@ public class MessageUtility {
 	private MessageUtility() {
 	}
 
-	//f-g-h
+	//f-g-h-i
 	public static final String getMessage() {
-		//b-c-d-f-g-h
-		return "abc2-b-c-d-f-g-h";
+		//b-c-d-f-g-h-i
+		return "abc2-b-c-d-f-g-h-i";
 	}
 }
